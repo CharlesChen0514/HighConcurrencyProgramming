729de062b4db73a0515bcd0a4551b89e32f6a0f9
CharlesChen0514 <chenjiale1006@outlook.com>
2023-05-10 13:56:46 +0800

    TPS 极限测试类

---
+++ src/test/java/LimitTest.java
@@ -0,0 +1,37 @@
+import lombok.extern.slf4j.Slf4j;
+import org.bitkernel.Task;
+
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+public class LimitTest {
+    public static void main(String[] args) {
+        long c = 0;
+        ByteBuffer buffer = ByteBuffer.allocate(32);
+        MessageDigest md = Task.getMessageDigestInstance();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        long time = 0;
+        logger.debug("Start time: {}", System.currentTimeMillis());
+        while (true) {
+            long start = System.nanoTime();
+            int x = random.nextInt(65535);
+            int y = random.nextInt(65535);
+            Task.executeTask(md, buffer, x, y);
+            c += 1;
+            buffer.clear();
+            if (time > 60L * 1000 * 1000000) {
+                break;
+            }
+            long end = System.nanoTime();
+            time += (end - start);
+        }
+
+        long tps = c / 60;
+        logger.info("The TPS of a single CPU is: {}", tps);
+        int processors = Runtime.getRuntime().availableProcessors();
+        logger.info("The TPS of {} CPU is: {}", processors, tps * processors);
+        logger.debug("End time: {}", System.currentTimeMillis());
+    }
+}
